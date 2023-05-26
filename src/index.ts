49e432969ad6ba501b37e679252a53a6b745503e
Shammael <shammamnd2015@gmail.com>
2023-05-26 10:21:31 -0400

    feat: :sparkles: latest

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import express from 'express';
 import morgan from 'morgan';
+import { PrismaClient } from '@prisma/client';
+
+// Postgres error command npx prisma migrate reset
+
+const client = new PrismaClient();
 
 const app = express();
 
@@ -7,8 +12,26 @@ app.use(morgan('dev'));
 
 const port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-  res.json({ ok: 'hola' });
+app.get('/create', async (req, res) => {
+  const user = await client.user.create({
+    data: {
+      age: 22,
+      email: 'shammamnd2015@gmail.com',
+      name: 'Shammael Bien-Aisé',
+      userPreferences: {
+        create: {
+          emailUpdates: true,
+        },
+      },
+    },
+  });
+
+  res.json({ user });
+});
+
+app.get('/', async (req, res) => {
+  const users = await client.user.findMany();
+  res.json({ users });
 });
 
 app.listen(port, () => {
